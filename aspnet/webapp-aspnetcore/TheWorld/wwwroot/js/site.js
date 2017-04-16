aceaf92399b5be26396cdf4dac6d5226e145013f
haydenmcp <hayden.mcparlane@gmail.com>
2017-04-16 15:48:51 -0500

    Added JQuery code, some css and html

--- aspnet/webapp-aspnetcore/TheWorld/wwwroot/js/site.js
+++ aspnet/webapp-aspnetcore/TheWorld/wwwroot/js/site.js
@@ -2,10 +2,24 @@ $(document).ready(function(){
     var main = $("#main");    
     
     main.mouseenter(function(){
-        main.css("background-color", "#888");        
+        main.css("background-color", "#888");                       
     }); 
 
     main.mouseleave(function(){
         main.css("background-color", "");        
-    });  
+    });      
+
+    var sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function(){
+        sidebarAndWrapper.toggleClass("hide-sidebar");
+        var toggleButton = $(this);
+        switch(sidebarAndWrapper.hasClass("hide-sidebar")){
+            case true: toggleButton.text("Show Menu"); break;
+            case false: toggleButton.text("Hide Menu"); break;
+            default:
+                alert("Logical Error: hide-sidebar should be either present or not.")
+        }
+    }); 
+
+
 });
